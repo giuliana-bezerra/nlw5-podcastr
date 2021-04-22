0feb9b4bbb4a218d2b8f005f9613e74d03046b5b
Giuliana Silva Bezerra <giu.drawer@gmail.com>
2021-04-22 19:55:46 -0300

    aula #3

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
-import styles from './styles.module.scss';
 import format from 'date-fns/format';
 import ptBR from 'date-fns/locale/pt-BR';
+import styles from './styles.module.scss';
 
 export default function Header() {
   const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
