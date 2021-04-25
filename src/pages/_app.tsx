9bd27359cca04ea3815b5047f3e9e06da877d142
Giuliana Silva Bezerra <giu.drawer@gmail.com>
2021-04-25 11:40:40 -0300

    aula #5

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,48 @@ import Header from '../components/Header';
 import '../styles/global.scss';
 import styles from '../styles/app.module.scss';
 import Player from '../components/Player';
+import { PlayerContext } from '../contexts/PlayerContext';
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function play(episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function togglePlay() {
+    console.log('Toggle Play');
+    setIsPlaying(!isPlaying);
+  }
+
+  function playFromKeyboard(isPlayingProp: boolean) {
+    setIsPlaying(isPlayingProp);
+  }
+
   return (
-    <div className={styles.wrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
+    <PlayerContext.Provider
+      value={{
+        episodeList,
+        currentEpisodeIndex,
+        play,
+        isPlaying,
+        togglePlay,
+        playFromKeyboard,
+      }}
+    >
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContext.Provider>
   );
 }
 
